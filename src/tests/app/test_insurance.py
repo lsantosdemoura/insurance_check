27e00de0491caccf01235d5d536e57480a8f11df
Lucas Moura <lsantosdemoura99@gmail.com>
2022-01-30 23:18:11 -0300

    Adding tests to rest of the controllers.

---
+++ src/tests/app/test_insurance.py
@@ -0,0 +1,57 @@
+from unittest import mock
+
+import pytest
+
+from src.app.insurance import (
+    _calculate_base_score,
+    _calculate_insurance_score,
+    calculate_insurance,
+)
+from src.models.models import Insurance, UserScoreEligibility
+
+
+@pytest.mark.parametrize(
+    "risk_questions,expected_value",
+    [
+        ([True, True, True], 3),
+        ([True, True, False], 2),
+        ([True, False, False], 1),
+    ],
+)
+def test_calculate_base_score_returns_sum_of_true_values(
+    risk_questions, expected_value
+):
+    user_score = _calculate_base_score(risk_questions=risk_questions)
+    user_score.disability.score == expected_value
+    user_score.auto.score == expected_value
+    user_score.home.score == expected_value
+    user_score.life.score == expected_value
+
+
+def test_calculate_insuranse_calls_all_passed_rules(user_input_base):
+    rule_1 = mock.Mock(side_effect=lambda user_input, user_score: user_score)
+    rule_2 = mock.Mock(side_effect=lambda user_input, user_score: user_score)
+    rules = [rule_1, rule_2]
+    calculate_insurance(user_input=user_input_base, rules=rules)
+    rule_1.assert_called_once()
+    rule_2.assert_called_once()
+
+
+def test_calculate_insurance_score_returns_ineligible_for_eligible_false():
+    score_value = UserScoreEligibility(score=3, eligible=False)
+    assert _calculate_insurance_score(score_value) == Insurance.INELIGIBLE
+
+
+@pytest.mark.parametrize(
+    "user_score,expected_insurance",
+    [
+        (UserScoreEligibility(score=0, eligible=True), Insurance.ECONOMIC),
+        (UserScoreEligibility(score=1, eligible=True), Insurance.REGULAR),
+        (UserScoreEligibility(score=3, eligible=True), Insurance.RESPONSIBLE),
+    ],
+)
+def test_calculate_insurance_score_returns_correct_for_each_score_range(
+    user_score, expected_insurance
+):
+
+    assert _calculate_insurance_score(user_score) == expected_insurance
---
+++ src/tests/controllers/test_handler.py
@@ -0,0 +1,39 @@
+from unittest import mock
+
+from src.app.rules import (
+    age_above_30_below_40,
+    age_above_60,
+    age_below_30,
+    does_not_have_a_house,
+    does_not_have_a_vehicle,
+    does_not_have_income,
+    has_dependents,
+    income_above_200k,
+    is_married,
+    mortgaged_house,
+    vehicle_older_than_5_years,
+)
+from src.controllers.handler import get_insurances
+
+
+@mock.patch("src.controllers.handler.calculate_insurance")
+def test_get_insurances_calls_calculate_insurance_with_all_rules(
+    mock_calculate_insurance, user_input_base
+):
+    get_insurances(user_input=user_input_base)
+    mock_calculate_insurance.assert_called_with(
+        user_input=user_input_base,
+        rules=(
+            age_above_60,
+            age_below_30,
+            age_above_30_below_40,
+            does_not_have_a_vehicle,
+            has_dependents,
+            income_above_200k,
+            is_married,
+            does_not_have_a_house,
+            does_not_have_income,
+            mortgaged_house,
+            vehicle_older_than_5_years,
+        ),
+    )
